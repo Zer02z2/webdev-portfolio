64a2b9b5658ade17b5b369f7c1f96c6d8f4f6670
Zer02z2 <zongzechan@gmail.com>
2024-10-05 22:39:54 -0400

    Google Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css"
 import { Aura } from "./_components/aura"
 import { Footer } from "./_footer"
 import { Header } from "./_header"
+import Script from "next/script"
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-inter" })
 
@@ -20,6 +21,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-9ZNY3953F4"
+        ></Script>
+        <Script id="google analytics">
+          {" "}
+          {`window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-9ZNY3953F4');`}
+        </Script>
+      </head>
       <body className={`${inter.className}`}>
         <div id="top-anchor" className="h-0"></div>
         <Aura />
